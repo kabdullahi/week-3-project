7e5ab59874c48ac5c87c9f3ab5d527b4f8a44bb5
kabdullahi <kzabdull@gmail.com>
2017-06-12 10:45:23 -0400

    inital commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const sevenBtn = document.querySelector("#seven");
+const eightBtn = document.querySelector("#eight");
+let btnList = document.querySelectorAll(".squares");
+
+
+for (let i = 0; i < btnList.length; i++) {
+  let btn = btnList[i];
+
+  btn.addEventListener("click", function(e) {
+
+  });
+}
